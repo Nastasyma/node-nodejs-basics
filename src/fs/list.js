3f0e20135cdb907417c87686c5f0548d2774387b
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-24 20:10:27 +0300

    feat: add solution for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,16 @@
+import fs from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = new URL("files", import.meta.url);
+
+    try {
+        await fs.access(folderPath);
+    } catch (err) {
+        throw new Error("FS operation failed");
+    }
+
+    const files = await fs.readdir(folderPath);
+    console.table(files);
 };
 
 await list();
