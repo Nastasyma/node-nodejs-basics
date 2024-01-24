feae15b1c554d417c4b32938943b56c4ac705a7e
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-24 19:00:57 +0300

    feat: add solution for rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,27 @@
+import * as url from "url";
+import path from "path";
+import fs from "fs";
+
 const rename = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    const srcFile = path.join(__dirname, "files", "wrongFilename.txt");
+    const destFile = path.join(__dirname, "files", "properFilename.md");
+
+    const fileExists = fs.existsSync(srcFile);
+    if (!fileExists) {
+        throw new Error("FS operation failed");
+    }
+
+    const destFileExists = fs.existsSync(destFile);
+    if (destFileExists) {
+        throw new Error("FS operation failed");
+    }
+
+    fs.rename(srcFile, destFile, (err) => {
+        if (err) {
+            throw new Error("FS operation failed");
+        }
+    })
 };
 
 await rename();
