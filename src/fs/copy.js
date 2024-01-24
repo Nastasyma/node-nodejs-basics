bd97b9cb511b3cf579bb529180db53a1668d773f
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-24 16:16:08 +0300

    feat: add solution for copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,37 @@
+import * as url from "url";
+import path from "path";
+import fs from "fs";
+
 const copy = async () => {
-    // Write your code here 
+    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    const srcFolder = path.join(__dirname, "files");
+    const destFolder = path.join(__dirname, "files_copy");
+
+    const folderExists = fs.existsSync(destFolder);
+    if (folderExists) {
+        throw new Error("FS operation failed");
+    }
+
+    fs.mkdir(destFolder, { recursive: true }, (err) => {
+        if (err) {
+            throw new Error("FS operation failed");
+        }
+        console.log('The folder "files-copy" has been created');
+    });
+
+    fs.readdir(srcFolder, (err, files) => {
+        if (err) {
+            throw new Error("FS operation failed");
+        }
+        files.forEach((file) => {
+            fs.copyFile(path.join(srcFolder, file), path.join(destFolder, file), (err) => {
+                if (err) {
+                    throw new Error("FS operation failed");
+                }
+                console.log(`${file} was copied to "files-copy" folder`);
+            });
+        });
+    });
 };
 
 await copy();
