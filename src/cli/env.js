864b0627d74bd796dc68c06ae3af581b2ef9a791
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-25 00:01:07 +0300

    feat: add solution for env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+    const results = [];
+    for (const key in process.env) {
+        if (key.startsWith('RSS_')) {
+            results.push(`${key}=${process.env[key]}`);
+        }
+    }
+    const output = results.join('; ');
+    console.log(output);
 };
 
 parseEnv();
