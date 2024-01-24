9b93394f244f72dd31b2089481c20040fb53905e
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-25 01:31:25 +0300

    refactor: delete existsSync from rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -7,21 +7,11 @@ const rename = async () => {
     const srcFile = path.join(__dirname, "files", "wrongFilename.txt");
     const destFile = path.join(__dirname, "files", "properFilename.md");
 
-    const fileExists = fs.existsSync(srcFile);
-    if (!fileExists) {
+    try {
+        await fs.rename(srcFile, destFile);
+    } catch (err) {
         throw new Error("FS operation failed");
     }
-
-    const destFileExists = fs.existsSync(destFile);
-    if (destFileExists) {
-        throw new Error("FS operation failed");
-    }
-
-    await fs.rename(srcFile, destFile, (err) => {
-        if (err) {
-            throw new Error("FS operation failed");
-        }
-    })
 };
 
 await rename();
