c996768f6d743991f9b9452fa302e2e9c7c7ca2e
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-24 19:50:44 +0300

    feat: add solution for delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import { access } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = new URL("files/fileToRemove.txt", import.meta.url);
+
+    access(filePath, (err) => {
+        if (err) {
+            throw new Error("FS operation failed");
+        }
+
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                throw new Error("FS operation failed");
+            }
+        });
+    });
 };
 
 await remove();
