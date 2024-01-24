53293a5f9b1743e31ad7d5b44c4b80c71028d90d
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-24 23:26:08 +0300

    feat: add solution for read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,15 @@
+import fs from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = new URL("files/fileToRead.txt", import.meta.url);
+
+    try {
+        await fs.access(filePath);
+        const content = await fs.readFile(filePath, "utf-8");
+        console.log(content);
+    } catch (err) {
+        throw new Error("FS operation failed");
+    }
 };
 
 await read();
