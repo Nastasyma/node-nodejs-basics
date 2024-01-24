d628ac430aed53f7f1f89b7e9ca820453cb9db8f
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-24 15:31:18 +0300

    feat: add solution for create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,14 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    console.log('Hello world!');
+
+    const filePath = new URL("files/fresh.txt", import.meta.url);
+
+    try {
+        await fs.writeFile(filePath, "I am fresh and young", { flag: 'wx' });
+    } catch (error) {
+        throw new Error('FS operation succeeded');
+    }
 };
 
 await create();
