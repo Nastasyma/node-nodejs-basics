f5a5941d798ddf9fdcb4b4da1418a6c42cbb4e86
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-27 13:45:13 +0300

    refactor: edit streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,12 +1,12 @@
 import * as url from "url";
 import path from "path";
-import { createReadStream } from 'fs';
+import fs from 'fs';
 
 const read = async () => {
     const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
     const filePath = path.join(__dirname, "files", "fileToRead.txt");
 
-    const stream = createReadStream(filePath, "utf-8");
+    const stream = fs.createReadStream(filePath, "utf-8");
 
     stream.on("data", (chunk) => {
         process.stdout.write(chunk);
