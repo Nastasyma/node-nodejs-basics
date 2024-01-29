e7e040d7e13c757463df54703ba7de16aada1769
Anastasia Shishmareva <stacy.shishmareva@gmail.com>
2024-01-29 10:46:18 +0300

    feat: add solution for wt/worker.js, wt/main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import * as url from "url";
+import path from "path";
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+    const filePath = path.join(__dirname, "./worker.js");
+
+    const cores = cpus().length;
+
+    const runWorker = (data) =>
+        new Promise((resolve, reject) => {
+            const worker = new Worker(filePath, { workerData: data });
+
+            worker.on("message", (data) => {
+                resolve({ status: "resolved", data });
+            });
+
+            worker.on("error", () => {
+                reject({ status: "error", data: null });
+            });
+        });
+
+    const workerPromises = Array.from({ length: cores }, (_, i) =>
+        runWorker(10 + i)
+    );
+    const results = await Promise.all(workerPromises);
+
+    console.log(results);
 };
 
 await performCalculations();
